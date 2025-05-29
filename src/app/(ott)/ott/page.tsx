eb747578b345687c847c6de66dbf65fb8739df1b
Sahabul Mondal <sahabulmondal2026@gmail.com>
2025-05-29 17:14:22 +0000

    If I open in other browser there is no redirection but in chrome it's getting redirect

--- src/app/(ott)/ott/page.tsx
+++ src/app/(ott)/ott/page.tsx
@@ -65,7 +65,7 @@ export default function OttPage() {
         allowFullScreen
         onLoad={handleLoad}
         onError={handleError}
-        sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts" // Removed allow-top-navigation and allow-top-navigation-by-user-activation
+        sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts" // Reinstated allow-scripts
       />
     </div>
   );
